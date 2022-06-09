b775bb35846e061e58b7b7adfbc82aef27f01537
hervechangue <rv_changue@hotmail.com>
2022-06-08 19:52:13 -1000

    react lifecycle + fetch api

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,24 +6,34 @@ import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
     state = {
-        todos: [
-            {
-                id: uuidv4(),
-                title: "Setup development environment",
-                completed: true
-            },
-            {
-                id: uuidv4(),
-                title: "Develop website and add content",
-                completed: false
-            },
-            {
-                id: uuidv4(),
-                title: "Deploy to live server",
-                completed: false
-            }
-        ]
+        todos: []
     };
+
+    /*componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+        .then(response => {console.log(response); return response.json()})
+        .then(data => this.setState({ todos: data }));
+    }*/
+
+    componentDidMount() {
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if (loadedTodos) {
+            /* Calling the setState() in this method would trigger an extra rendering. That is, the render() will be called twice. Though it’s fine because it will happen before the browser updates the view. However, we should always use with caution or simply avoid it if you could to prevent any performance issue.
+Since in our case, we want to display the stored item(s) after component render, we can simply assign the item(s) directly as the initial state.*/
+            this.setState({
+                todos: loadedTodos
+            })
+        }
+    }
+
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.todos !== this.state.todos) {
+            const temp = JSON.stringify(this.state.todos)
+            localStorage.setItem("todos", temp)
+        }
+    }
 
     /*handleChange = id => {
         this.setState({
@@ -92,14 +102,14 @@ class TodoContainer extends React.Component {
     };
     setUpdate = (updatedTitle, id) => {
         this.setState({
-          todos: this.state.todos.map(todo => {
-            if (todo.id === id) {
-              todo.title = updatedTitle
-            }
-            return todo
-          }),
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.title = updatedTitle
+                }
+                return todo
+            }),
         })
-      }
+    }
 
     render() {
         return (
