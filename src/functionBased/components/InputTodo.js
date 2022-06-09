e6ff47d812b818a08158dcc2e1ee5b3e00729fdd
hervechangue <rv_changue@hotmail.com>
2022-06-08 20:43:02 -1000

    functionBase does not merge states between updates

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -34,3 +34,62 @@ const InputTodo = props => {
     )
 }
 export default InputTodo
+
+/*
+Unlike the class-based approach. Anytime you have an object as the state value, React will not merge the state returned by the useState Hook with that of the update passed to it.
+Meaning it doesn’t merge the old and new state. Instead, it overrides the entire state with that of the current.
+*/
+
+/*
+import React, { useState } from "react"
+
+const InputTodo = props => {
+  const [inputText, setInputText] = useState({
+    fName: "",
+    lastName: "",
+  })
+
+  const onChange = e => {
+    setInputText(prevState => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log("submitted")
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add first name"
+          value={inputText.fName}
+          name="fName"
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add last name"
+          value={inputText.lastName}
+          name="lastName"
+          onChange={onChange}
+        />
+
+        <button className="input-submit">Submit</button>
+      </form>
+      <h2>{inputText.fName}</h2>
+      <h2>{inputText.lastName}</h2>
+    </>
+  )
+}
+
+export default InputTodo
+*/
