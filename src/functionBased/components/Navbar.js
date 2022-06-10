3b4d384d179bdba08592fdf25681d17de488b99a
hervechangue <rv_changue@hotmail.com>
2022-06-09 21:19:47 -1000

    hamburger button

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,7 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, NavLink } from "react-router-dom"
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
 
 const Navbar = () => {
+
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+    const handleToggle = () => {
+        setNavbarOpen(prev => !prev)
+    }
+
+    const closeMenu = () => {
+        setNavbarOpen(false)
+    }
+
     const links = [
         {
             id: 1,
@@ -17,13 +30,26 @@ const Navbar = () => {
 
     return (
         <nav className="navBar">
-            <ul>
+            <button onClick={handleToggle}>
+                {navbarOpen ? (
+                    <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+                ) : (
+                    <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+                )}
+            </button>
+
+            <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
                 {links.map(link => {
                     return (
                         <li key={link.id}>
                             {/* <Link to={link.path}>{link.text}</Link> */}
 
-                            <NavLink to={link.path}>{link.text}</NavLink>
+                            <NavLink
+                                to={link.path}
+                                onClick={() => closeMenu()}
+                            >
+                                {link.text}
+                            </NavLink>
                         </li>
                     )
                 })}
